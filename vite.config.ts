f33fb7826477d73d7d6cab761ec0e4a84bbef6d5
William Wang <williamwang921220@gmail.com>
2024-10-15 13:44:55 -0700

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // Replace with your actual repository name
+  base: '/wiliwangka.github.io/', // Replace with your actual repository name
 });
